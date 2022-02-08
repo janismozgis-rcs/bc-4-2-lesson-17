e916f84fb721c128ff3bb26cad453df3ca24c089
janismozgis-rcs <janismozgis+rcs@gmail.com>
2022-02-08 20:56:13 +0200

    Add lesson stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Alert from "./Components/Alert";
+import Counter from "./Components/Counter";
+import FancyInput from "./Components/FancyInput";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Alert />
+            <Counter />
+            <FancyInput />
+        </div>
+    );
 }
 
 export default App;
